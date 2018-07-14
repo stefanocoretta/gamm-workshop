54afcd0fa0ce7ab6bf276e66e8a197732276ffb8
Stefano Coretta <stefanoc.social@me.com>
2018-07-14 10:36:25 +0200

    add plot bs

---
+++ plot-bs.R
@@ -0,0 +1,23 @@
+library(mgcv)
+x1 <- 1:1000
+um <- smoothCon(
+  s(x1),
+  data = data.frame(x1 = sort(x1)),
+  knots = NULL,
+  absorb.cons = TRUE
+)
+X <- um[[1]]$X
+plot(sort(x1), X[, 1], ylim = range(X), type = "l")
+for (i in 2:ncol(X))
+  lines(sort(x1), X[, i], col = i)
+
+um <- smoothCon(
+  s(x1, bs = "ds"),
+  data = data.frame(x1 = sort(x1)),
+  knots = NULL,
+  absorb.cons = TRUE
+)
+X <- um[[1]]$X
+plot(sort(x1), X[, 1], ylim = range(X), type = "l")
+for (i in 2:ncol(X))
+  lines(sort(x1), X[, i], col = i)
