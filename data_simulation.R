d32ae85ad424fef423831c2c90027e2eea10da50
Stefano Coretta <stefanoc.social@me.com>
2018-07-16 09:56:02 +0200

    try different data

--- data_simulation.R
+++ data_simulation.R
@@ -1,8 +1,8 @@
-set.seed(20160227)
+set.seed(4321)
 x <- seq(0,100,10)
-y <- ((runif(1,10,20)*x)/(runif(1,0,10) + x)) + rnorm(11,0,2)
+y <- ((runif(1,10,20)*x)/(runif(1,0,10) + x)) + rnorm(11,0,1)
 x_1 <- seq(0,100,10)
-y_1 <- ((runif(1,10,20)*x_1)/(runif(1,0,100) + x_1)) + rnorm(11,0,2)
+y_1 <- ((runif(1,10,20)*x_1)/(runif(1,0,100) + x_1)) + rnorm(11,0,1)
 
 test <- tibble(
   x = c(x, x_1),
@@ -25,3 +25,13 @@ ggplot(test, aes(x, y, group = g)) +
 ggplot(test, aes(x, y, group = g)) +
   geom_point() +
   geom_smooth()
+
+filter(test, g == "a") %>%
+  ggplot(aes(x, y)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y ~ x + I(x^2) + I(x^3) + I(x^4) + I(x^5) + I(x^6) + I(x^7) + I(x^8) + I(x^9) + I(x^10))
+
+filter(test, g == "a") %>%
+  ggplot(aes(x, y)) +
+  geom_point() +
+  geom_smooth(method = "gam", formula = y ~ s(x))
