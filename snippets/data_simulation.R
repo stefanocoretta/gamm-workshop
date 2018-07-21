8f5d26b1de139d6a1e5ebf2d209b7d71e7038c48
Stefano Coretta <stefanoc.social@me.com>
2018-07-21 09:01:52 +0200

    fix code

--- snippets/data_simulation.R
+++ snippets/data_simulation.R
@@ -1,14 +1,17 @@
 set.seed(4321)
-x <- seq(0,100,10)
-y <- ((runif(1,10,20)*x)/(runif(1,0,10) + x)) + rnorm(11,0,1)
-x_1 <- seq(0,100,10)
-y_1 <- ((runif(1,10,20)*x_1)/(runif(1,0,100) + x_1)) + rnorm(11,0,1)
+x <- seq(0, 100, 10)
+y <- ((runif(1, 10, 20) * x) / (runif(1, 0, 10) + x)) + rnorm(11, 0, 1)
+x_1 <- seq(0, 100, 10)
+y_1 <- ((runif(1, 10, 20) * x_1) / (runif(1, 0, 200) + x_1)) + rnorm(11, 0, 1)
 
 test <- tibble(
   x = c(x, x_1),
   y = c(y, y_1),
   g = rep(c("a", "b"), each = 11)
 )
+
+ggplot(test, aes(x, y, colour = g)) +
+  geom_point()
 
 ggplot(test, aes(x, y, group = g)) +
   geom_point() +
@@ -35,3 +38,4 @@ filter(test, g == "a") %>%
   ggplot(aes(x, y)) +
   geom_point() +
   geom_smooth(method = "gam", formula = y ~ s(x))
+
